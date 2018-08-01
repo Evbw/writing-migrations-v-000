806bea70f6963eb9c153c725784495e7ac73ea6c
Everett <everett.bowline@gmail.com>
2018-08-01 01:50:40 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -3,7 +3,15 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
       t.string :name
+<<<<<<< HEAD
     end
   end
   
 end
+=======
+      
+    end
+  end
+  
+end
+>>>>>>> f95be43af9bf6e54982ea85d5aa444a305be4c76
